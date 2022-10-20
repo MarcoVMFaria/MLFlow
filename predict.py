aa6bd42c88339a4bc8d9581e87bb3569e00dff27
marcopolo2000k@gmail.com <marcopolo2000k@gmail.com>
2022-10-20 15:01:17 +0000

    Fim do curso MLFlow

---
+++ predict.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+import mlflow
+model = mlflow.sklearn.load_model("models:/dota-unesp/production")
+
+sdf = spark.table("sandbox_apoiadores.abt_dota_pre_match_new")
+df = sdf.toPandas()
+
+# COMMAND ----------
+
+features = list(set(df.columns.tolist()) - set(["match_id", "radiant_win"]))
+                
+x = df[features]
+
+# COMMAND ----------
+
+score = model.predict_proba(x)
+
+df["proba_randian_win"] = score[:,1]
+df[["match_id", "radiant_win", "proba_randian_win"]]
